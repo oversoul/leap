42c1f5073627fee9e34b412fde40b2e2464df43e
Amine <kurosoul.ae@gmail.com>
2020-02-16 20:18:46 +0100

    create task class

---
+++ src/Task.php
@@ -0,0 +1,30 @@
+<?php
+namespace Aecodes\Leap;
+
+class Task {
+    protected $line;
+    protected $number;
+
+    public function __construct(string $line, int $number) {
+        $this->line = $line;
+        $this->number = $number;
+    }
+
+    public function getLine()
+    {
+        return trim($this->line);
+    }
+
+    public function getLineNumber()
+    {
+        return $this->number;
+    }
+
+    public function renderLine($size = 12, $word = 'line')
+    {
+        $string = "{$word} {$this->number}";
+        $spaces = str_repeat(' ', $size - strlen($string));
+    
+        return $string . $spaces;
+    }
+}
