d3b44f64a29a7c8a6f3f63b263aacb34d4936990
Amine <kurosoul.ae@gmail.com>
2020-02-16 20:18:28 +0100

    feat: parse files and register tasks

---
+++ src/FileParser.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Aecodes\Leap;
+
+class FileParser
+{
+    protected $tasks = [];
+    protected $keywords = ['FIXME', 'NOTE'];
+
+    /**
+     * Parse all files
+     *
+     * @param iterable $files
+     * @return void
+     */
+    public function parseFiles(iterable $files)
+    {
+        foreach ($files as $file) {
+            $this->parseFile($file->getRealPath());
+        }
+    }
+
+    /**
+     * Parse the content of a file
+     *
+     * @param string $file
+     * @return void
+     */
+    public function parseFile(string $file)
+    {
+        $content = file_get_contents($file);
+        $lines = explode("\n", $content);
+        
+        for($index = 0; $index < count($lines); $index++ ) {
+            // line numbers are 1 based. index is 0 based
+            $this->parseLine($file, $lines[$index], $index + 1);
+        }
+    }
+    
+    /**
+     * Parse line
+     *
+     * @param string $file
+     * @param string $line
+     * @param int $lineNumber
+     * @return void
+     */
+    public function parseLine(string $file, string $line, int $lineNumber)
+    {
+        if ($this->containTask($line)) {
+            $this->registerTask($file, $line, $lineNumber);
+        }
+    }
+
+    /**
+     * check if line contains a task
+     *
+     * @param string $line
+     * @return bool
+     */
+    public function containTask(string $line): bool
+    {
+        if ( strlen($line) < 4 ) return false;
+
+        // running stripos on all keywords
+        $result = array_filter($this->keywords, function($key) use($line) {
+            return stripos($line, $key . ':') !== false;
+        });
+
+        return ( count($result) > 0 );
+    }
+
+    /**
+     * Register task
+     *
+     * @param string $file
+     * @param string $line
+     * @param int $lineNumber
+     * @return void
+     */
+    public function registerTask(string $file, string $line, int $lineNumber): void
+    {
+        $line = substr($line, 0, 100);
+        $this->tasks[$file][] = new Task($line, $lineNumber);
+    }
+
+    /**
+     * Get all registered tasks
+     *
+     * @return array
+     */
+    public function results(): array
+    {
+        return $this->tasks;
+    }
+}
---
+++ tests/FileParserTest.php
@@ -0,0 +1,69 @@
+<?php
+namespace Aecodes\Leap\Tests;
+
+use Aecodes\Leap\FileParser;
+use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\TestCase;
+
+class FileParserTest extends TestCase {
+    
+
+    public function testContainTask()
+    {
+        $parser = new FileParser;
+        
+        $result = $parser->containTask('hello world // Note: this is a task');
+        $this->assertTrue($result);
+
+        $result = $parser->containTask('hello world // NOTE: this is a task');
+        $this->assertTrue($result);
+        
+        $result = $parser->containTask('hello world // Not');
+        $this->assertFalse($result);
+
+                
+        $result = $parser->containTask('hello world // FIXME: fix this');
+        $this->assertTrue($result);
+    }
+
+    public function testCanParseFile()
+    {
+        $directory = [
+            'file.php' => '<?php 
+                // Note: update this file
+            ',
+            'second-file.js' => '
+                // FIXME: fix this function
+                doSomeStuff() {
+
+                }
+
+                // FIXME: whaaatever
+            ',
+            'no-task-file' => '
+                lorem
+                ipsum
+            '
+        ];
+        // setup and cache the virtual file system
+        $fs = vfsStream::setup('root', 444, $directory);
+
+        $parser = new FileParser;
+        $filePath = $fs->url() . '/file.php';
+        $parser->parseFile($filePath);
+        $this->assertArrayHasKey($filePath, $parser->results());
+        $this->assertCount(1, $parser->results()[$filePath]);
+        
+        $parser = new FileParser;
+        $filePath = $fs->url() . '/second-file.js';
+        $parser->parseFile($filePath);
+        $this->assertArrayHasKey($filePath, $parser->results());
+        $this->assertCount(2, $parser->results()[$filePath]);
+        
+        $parser = new FileParser;
+        $filePath = $fs->url() . '/no-task-file';
+        $parser->parseFile($filePath);
+        $this->assertArrayNotHasKey($filePath, $parser->results());
+    }
+
+}
